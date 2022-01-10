39bb4439d6f73dc0156de4642d5eb8229958d9a6
Dat <trandat251111@gmail.com>
2022-01-10 21:28:31 +0700

    add2

---
+++ js1.js
@@ -0,0 +1,5 @@
+var text = ""
+for (let i = 0; i < 6; i++) {
+    text += "The number is: " + i +"<br>";
+}
+document.getElementById("result").innerHTML = text;
---
+++ js2.js
@@ -0,0 +1,6 @@
+let num = prompt("Enter your number: ");
+let total = 0;
+for (let i = 1; i <= num; i +=1) {
+    total = total + i;
+}
+alert(total);
